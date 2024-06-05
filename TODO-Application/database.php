9447ce7c9d3ff8043dedc77feb4aa7e1d1a469ef
lukijanki <121889156+lukijanki@users.noreply.github.com>
2024-06-05 08:59:23 +0200

    Update database.php
    database settings

--- TODO-Application/database.php
+++ TODO-Application/database.php
@@ -43,7 +43,7 @@
     }
 
     function connectdatabase() {
-        return mysqli_connect("127.0.0.1:3306", "dhiraj", "pass.1.11", "todo");
+        return mysqli_connect("todolist-lab12-server", "wacztqewss", "9SUC$XDzNb6WI8CU", "todolist-lab12-database");
     }
 
     function loggedin() {
